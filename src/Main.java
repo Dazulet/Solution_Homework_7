97691cf6fc3a21d08845294f9838f5ae4540c08b
Daulet <baltashdaulet3@gmail.com>
2025-04-25 17:14:52 +0500

    i implement Demonstration Iterator Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Season s1 = new Season();
+        s1.addEpisode(new Episode("S1E1", 1200));
+        s1.addEpisode(new Episode("S1E2", 1100));
+
+        Season s2 = new Season();
+        s2.addEpisode(new Episode("S2E1", 1300));
+        s2.addEpisode(new Episode("S2E2", 1250));
+
+        Series series = new Series();
+        series.addSeason(s1);
+        series.addSeason(s2);
+
+        System.out.println("== Normal Order ==");
+        EpisodeIterator normal = s1.createIterator();
+        while (normal.hasNext()) {
+            System.out.println(normal.next());
+        }
+
+        System.out.println("\n== Reverse Order ==");
+        EpisodeIterator reverse = s1.createReverseIterator();
+        while (reverse.hasNext()) {
+            System.out.println(reverse.next());
+        }
+
+        System.out.println("\n== Shuffle Order ==");
+        EpisodeIterator shuffle = s1.createShuffleIterator(42);
+        while (shuffle.hasNext()) {
+            System.out.println(shuffle.next());
+        }
+
+        System.out.println("\n== Binge All Episodes ==");
+        EpisodeIterator binge = series.createBingeIterator();
+        while (binge.hasNext()) {
+            System.out.println(binge.next());
+        }
     }
 }
