c08de783c007b45c0b58623cf12a24a14f689d62
Daulet <baltashdaulet3@gmail.com>
2025-04-25 17:06:12 +0500

    i implement BingeIterator,ReverseSeasonIterator,ShuffleSeasonIterator

--- src/BingeIterator.java
+++ src/BingeIterator.java
@@ -1,2 +1,30 @@
-public class BingeIterator {
+import java.util.List;
+
+public class BingeIterator implements EpisodeIterator {
+    private List<Season> seasons;
+    private int seasonIndex = 0;
+    private EpisodeIterator currentIterator;
+
+    public BingeIterator(List<Season> seasons) {
+        this.seasons = seasons;
+        if (!seasons.isEmpty()) {
+            currentIterator = seasons.get(0).createIterator();
+        }
+    }
+
+    public boolean hasNext() {
+        while (currentIterator != null && !currentIterator.hasNext()) {
+            seasonIndex++;
+            if (seasonIndex < seasons.size()) {
+                currentIterator = seasons.get(seasonIndex).createIterator();
+            } else {
+                return false;
+            }
+        }
+        return currentIterator != null && currentIterator.hasNext();
+    }
+
+    public Episode next() {
+        return currentIterator.next();
+    }
 }
--- src/ShuffleSeasonIterator.java
+++ src/ShuffleSeasonIterator.java
@@ -1,2 +1,19 @@
-public class ShuffleSeasonIterator {
+import java.util.*;
+
+public class ShuffleSeasonIterator implements EpisodeIterator {
+    private List<Episode> shuffled;
+    private int index = 0;
+
+    public ShuffleSeasonIterator(List<Episode> episodes, long seed) {
+        shuffled = new ArrayList<>(episodes);
+        Collections.shuffle(shuffled, new Random(seed));
+    }
+
+    public boolean hasNext() {
+        return index < shuffled.size();
+    }
+
+    public Episode next() {
+        return shuffled.get(index++);
+    }
 }
